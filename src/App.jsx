a3c65c6cd3020d5ff9d8affb88f1a38acfcdd1e1
Jagadeshwaran P <148187521+jaga-Innovation@users.noreply.github.com>
2024-11-14 13:01:34 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Greeting from "./Greeting";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const toggleLogin = () => {
+    setIsLoggedIn(!isLoggedIn);
+  };
 
   return (
     <>
@@ -21,15 +27,12 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      <Greeting isLoggedIn={isLoggedIn} />
+      <button onClick={toggleLogin}>{isLoggedIn ? "Log out" : "Log in"}</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
